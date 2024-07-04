1f69f95e55d3f3d0d88e336820301ad550a011b7
kevmhughes <kevhughes24@hotmail.com>
2024-07-04 20:44:31 +0200

    Inlcude partials in home.ejs and form.ejs

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 const express = require("express");
 const morgan = require("morgan");
 
-// cteate an express instance
+// create an express instance
 const app = express();
 const port = 3000;
 
 // database
-const images = [];
+const images = [
+/*   {
+    title: "dog_2",
+    link: "https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
+    date: "2024-07-02",
+    tag: "animals",
+  },
+  {
+    title: "dog_1",
+    link: "https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg",
+    date: "2024-07-04",
+    tag: "animals",
+  }, */
+];
 
 app.set("view engine", "ejs");
 
@@ -29,11 +42,19 @@ app.get("/add-image-form", (req, res) => {
 
 // POST request to
 app.post("/add-image-form", (req, res) => {
-// redirect method of the response object
-  res.redirect("/");
-  images.push(req.body);
+  // redirect method of the response object
+  const urlUploaded = req.body.link;
+  const isUrlInDatabase = images.find((i) => i.link == urlUploaded);
+  console.log("is URL in database", isUrlInDatabase);
+  console.log("URL being uploaded", urlUploaded);
+  console.log(images);
+
+    images.push(req.body);
+    res.redirect("/");
+
 });
 
 app.listen(port, (req, res) => {
   console.log(`The server is running on port ${port}`);
 });
+
