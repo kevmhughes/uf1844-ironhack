b0e0c941c28399655306e97c0e5084f2deadcf32
kevmhughes <kevhughes24@hotmail.com>
2024-07-05 02:56:52 +0200

    Add basic get average colour of image functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import third party modules
 const express = require("express");
 const morgan = require("morgan");
+const getColors = require("get-image-colors");
 
 // create an express instance
 const app = express();
@@ -15,24 +16,48 @@ app.use(express.static("public"));
 
 // database
 const images = [
-  /*   {
-      title: "dog_2",
-      link: "https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg",
-      date: "2024-07-02",
-      category: "animals",
-    },
-    {
-      title: "dog_1",
-      link: "https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg",
-      date: "2024-07-04",
-      category: "animals",
-    }, */
+/*   {
+    title: "Tarsier",
+    link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/419044791_10160888975996001_7341947960153957418_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_ohc=XvSG2qlZ7ZAQ7kNvgE4jmuz&_nc_ht=scontent.fbcn14-1.fna&gid=A-dD64Y1zt-oR5YRDC5hgk5&oh=00_AYCyCEjtmAbW5zF6MrZAy7017kMV7ykB_AO6Bhd4lghN1A&oe=668D02B3",
+    date: "2024-07-04",
+    category: "animals",
+  },
+  {
+    title: "Coypu",
+    link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/417465834_10160826140581001_5317819119682968267_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=f727a1&_nc_ohc=QvleWTwcVYQQ7kNvgFQt4Hg&_nc_ht=scontent.fbcn14-1.fna&gid=A1uss7IQQ4C6zxc-6chQh16&oh=00_AYDUiMblrv6el_Mx07LU8JI7o5itJc4xXuFAc-PeV6e2uQ&oe=668CDCAC",
+    date: "2024-07-04",
+    category: "animals",
+  },
+  {
+    title: "Orangutan",
+    link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/367465051_10160512962351001_5626482259291271616_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=f727a1&_nc_ohc=9DWe2_JfUKAQ7kNvgECdigN&_nc_ht=scontent.fbcn14-1.fna&oh=00_AYAfitF1wjG4gL9WEg7gF2F8QngqGp8nC4Z1KdDoINznqw&oe=668CF458",
+    date: "2024-07-04",
+    category: "animals",
+  },
+  {
+    title: "Squirrel",
+    link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/449633607_10161118730131001_3614059652910363683_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_ohc=XOdKuPvZsjYQ7kNvgFkJSRW&_nc_ht=scontent.fbcn14-1.fna&gid=AFU7EaxoGUmLDbp5G1-V8-T&oh=00_AYBq_QgxbTp_So3KjRUNr3puqoNihKn2C3kgw9FTH1cu6g&oe=668CD801",
+    date: "2024-07-04",
+    category: "animals",
+  }, */
 ];
 
 app.set("view engine", "ejs");
 
+const getRgb = async (image) => {
+  const colors = await getColors(image.link, { count: 1 });
+  image.color = colors[0]._rgb.slice(0, 3).join(" ");
+  image.colorText = colors[0]._rgb.slice(0, 3);
+};
+
+const addRgbToImages = async (images) => {
+  await Promise.all(images.map((image) => getRgb(image)));
+};
+
 // GET request to render "/"
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  await addRgbToImages(images);
+  console.log(images)
   res.render("home", {
     images /* only one attribute is needed if the key is the same as the value =>  images: images, */,
   });
@@ -48,17 +73,15 @@ app.get("/add-image-form", (req, res) => {
 
 // POST request to
 app.post("/add-image-form", (req, res) => {
-  // redirect method of the response object
   const urlUploaded = req.body.link;
   const isUrlInDatabase = images.find((i) => i.link == urlUploaded);
-  console.log("is URL in database", isUrlInDatabase);
-  console.log("URL being uploaded", urlUploaded);
-  console.log(images);
 
+  // redirect method of the response object
   /* res.redirect("/"); */
 
   if (!isUrlInDatabase) {
     images.push(req.body);
+
     res.render("form", {
       isImagePosted: true,
       imageAlreadyAdded: undefined,
