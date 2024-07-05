61b5c83a5c534c0b10eec00da3f2a7e1b685e6f9
kevmhughes <kevhughes24@hotmail.com>
2024-07-06 01:33:06 +0200

    Add basic functionality to sort images by date and convert titles to uppercase

--- app.js
+++ app.js
@@ -15,36 +15,36 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // database
-const images = [
+let images = [
   {
     title: "Tarsier",
     link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/419044791_10160888975996001_7341947960153957418_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_ohc=XvSG2qlZ7ZAQ7kNvgE4jmuz&_nc_ht=scontent.fbcn14-1.fna&gid=A-dD64Y1zt-oR5YRDC5hgk5&oh=00_AYCyCEjtmAbW5zF6MrZAy7017kMV7ykB_AO6Bhd4lghN1A&oe=668D02B3",
-    date: "2024-07-04",
+    date: "2024-07-01",
     category: "animals",
   },
   {
     title: "Coypu",
     link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/417465834_10160826140581001_5317819119682968267_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=f727a1&_nc_ohc=QvleWTwcVYQQ7kNvgFQt4Hg&_nc_ht=scontent.fbcn14-1.fna&gid=A1uss7IQQ4C6zxc-6chQh16&oh=00_AYDUiMblrv6el_Mx07LU8JI7o5itJc4xXuFAc-PeV6e2uQ&oe=668CDCAC",
-    date: "2024-07-04",
+    date: "2024-07-06",
     category: "animals",
   },
   {
     title: "Orangutan",
     link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/367465051_10160512962351001_5626482259291271616_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=f727a1&_nc_ohc=9DWe2_JfUKAQ7kNvgECdigN&_nc_ht=scontent.fbcn14-1.fna&oh=00_AYAfitF1wjG4gL9WEg7gF2F8QngqGp8nC4Z1KdDoINznqw&oe=668CF458",
-    date: "2024-07-04",
+    date: "2024-07-05",
     category: "animals",
   },
   {
     title: "Squirrel",
     link: "https://scontent.fbcn14-1.fna.fbcdn.net/v/t39.30808-6/449633607_10161118730131001_3614059652910363683_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_ohc=XOdKuPvZsjYQ7kNvgFkJSRW&_nc_ht=scontent.fbcn14-1.fna&gid=AFU7EaxoGUmLDbp5G1-V8-T&oh=00_AYBq_QgxbTp_So3KjRUNr3puqoNihKn2C3kgw9FTH1cu6g&oe=668CD801",
-    date: "2024-07-04",
+    date: "2024-07-11",
     category: "animals",
   },
 ];
 
 app.set("view engine", "ejs");
 
-// async function that gets average color of an image, and then adds new properties 
+// async function that gets average color of an image, and then adds new properties
 const getRgb = async (image) => {
   // gets average color using the URL
   const colors = await getColors(image.link, { count: 1 });
@@ -54,6 +54,7 @@ const getRgb = async (image) => {
   image.colorText = colors[0]._rgb.slice(0, 3).join(", ");
 };
 
+// async arrow function
 const addRgbToImages = async (images) => {
   // maps images array of objects adding the new properties to all of the objects
   await Promise.all(images.map((image) => getRgb(image)));
@@ -61,7 +62,9 @@ const addRgbToImages = async (images) => {
 
 // GET request to render "/"
 app.get("/", async (req, res) => {
-  console.log(images)
+  // sort images by date from most recent to oldest 
+  images = images.sort((a , b) => new Date(b.date) - new Date(a.date))
+  // async 
   await addRgbToImages(images);
   res.render("home", {
     images /* only one attribute is needed if the key is the same as the value =>  images: images, */,
@@ -76,24 +79,23 @@ app.get("/add-image-form", (req, res) => {
   });
 });
 
-// POST request to
+// POST request to "/add-image-form"
 app.post("/add-image-form", (req, res) => {
-  const urlUploaded = req.body.link;
-  const isUrlInDatabase = images.find((i) => i.link == urlUploaded);
+  const urltoBeUploaded = req.body.link;
+  const isUrlInDatabase = images.find((i) => i.link == urltoBeUploaded);
 
   // redirect method of the response object
   /* res.redirect("/"); */
 
   if (!isUrlInDatabase) {
     images.push(req.body);
-
     res.render("form", {
       isImagePosted: true,
-      imageAlreadyAdded: undefined,
+      imageAlreadyAdded: false,
     });
   } else {
     res.render("form", {
-      isImagePosted: undefined,
+      isImagePosted: false,
       imageAlreadyAdded: true,
     });
   }
